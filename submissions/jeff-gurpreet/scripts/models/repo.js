88150c226fd1edf27bf8ad946b8d8f5e87b8dde9
Jeffrey Gebhardt <jeff@gebbey.com>
2016-05-24 13:07:10 -0700

    did first couple todos

---
+++ submissions/jeff-gurpreet/scripts/models/repo.js
@@ -0,0 +1,32 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  repos.requestRepos = function(callback) {
+    /* TODO: How would you like to fetch your repos? Someone say AJAX!?
+       Don't forget to call the callback! */
+    $.ajax({
+      url: 'https://api.github.com/users/jeffgebhardt/repos' +
+          '?per_page=10' +
+          '&sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + myToken},
+      success: function(data, message, xhr) {
+        console.log(data);
+      }
+    });
+  };
+
+  repos.with = function(attr) {
+    /* DONE: This Model method filters the full repos collection based
+        on a particular attribute. You could use this to filter all
+        repos that have a non-zero `forks_count`, `stargazers_count`,
+        or `watchers_count`. */
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
---
+++ submissions/jeff-gurpreet/scripts/models/token.js
@@ -0,0 +1,1 @@
+var myToken = '0a646b0af0f7f2d82cb7ec171cbbeced611e316f';
